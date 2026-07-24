d040e30eaeb4b4706e4a675b6d92c00a05781259
Baron Wang <hello@dbflow.dev>
2026-07-24 16:08:39 +0800

    chore: add copyright and license information to multiple PHP files
    Updated several PHP files to include copyright and license information for the dbflowlabs/filament package, ensuring compliance and clarity regarding ownership and usage rights.

--- src/Support/Presenters/RuntimeBadgePresenter.php
+++ src/Support/Presenters/RuntimeBadgePresenter.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This file is part of the dbflowlabs/filament package.
+ *
+ * Copyright (c) 2026 Baron Wang <hello@dbflow.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT
+ * @link    https://dbflow.dev
+ * @see     https://github.com/dbflow-labs/dbflow-filament
+ */
+
 declare(strict_types=1);
 
 namespace DbflowLabs\Filament\Support\Presenters;
